61e8d4a32e1c2c0569041884164c2a91207faef5
Emily N Mayeski <enmayeski@gmail.com>
2021-08-06 18:33:15 -0400

    Get API routes all function properly and show in main area for notes when clicked.

--- server.js
+++ server.js
@@ -5,26 +5,26 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const { notes } = require('./db/db');
+const { notes } = require("./db/db");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 //Generate Unique ID
-const generateUniqueId = require('generate-unique-id');
+// const generateUniqueId = require('generate-unique-id');
 
 
 //Create New Note
-function createNewNote(body, notesArray) {
-    const note = body;
-    notesArray.push(note);
-    fs.writeFileSync(
-      path.join(__dirname, './db/db.json'),
-      JSON.stringify({ notes: notesArray }, null, 2)
-    );
-    return note;
-  }
+// function createNewNote(body, notesArray) {
+//     const note = body;
+//     notesArray.push(note);
+//     fs.writeFileSync(
+//       path.join(__dirname, './db/db.json'),
+//       JSON.stringify({ notes: notesArray }, null, 2)
+//     );
+//     return note;
+//   }
   
 //Validate note contents
 // function validateNote(note) {
@@ -52,20 +52,21 @@ app.get('/notes', (req, res) => {
 });
 
 app.get('/api/notes', (req, res) => {
-    return res.json(notes);
+  // res.send("Test");
+    res.json(notes);
 });
 
 
-app.post('/api/notes', (req, res) => {
-    req.body.id = generateUniqueId();
+// app.post('/api/notes', (req, res) => {
+//     req.body.id = generateUniqueId();
 
-    // if (!validateNote(req.body)) {
-    //     res.status(400).send('You must enter both a title and a body.');
-    //   } else {
-        const note = createNewNote(req.body, notes);
-        res.json(note);
-    //   }
-    });
+//     if (!validateNote(req.body)) {
+//         res.status(400).send('You must enter both a title and a body.');
+//       } else {
+//         const note = createNewNote(req.body, notes);
+//         res.json(note);
+//       }
+//     });
     
 
 
