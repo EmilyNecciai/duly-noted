b3969038d25b29e258df78e6fa4743ee6faf8b1d
Emily N Mayeski <enmayeski@gmail.com>
2021-08-06 19:17:13 -0400

    adding readme content and cleaning up before pushing to heroku:

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# duly-noted
-Module 11 Challenge
+# Duly Noted - Note Taker App
+> This was done for the Module 11: Express.js Challenge. 
+
+## Table of Contents
+- [Description](#project-description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Testing](#testing)
+- [Questions](#questions)
+
+
+## Project Description
+This note-taker web app, Duly Noted, will allow anyone to title and write extensive notes and keep track of tasks, then save them for later reading or deletion. 
+
+### Live Site
+Check out the live site [here]()!
+
+### Demo 
+[Go to Demo]() (Use `Ctrl + Click` to open the video in a new tab.)
+
+This demo shows that you can open previous and create new notes. It also shows the creation of unique IDs using the NPM "[Generate Unique ID](https://www.npmjs.com/package/generate-unique-id)."
+
+## Installation 
+To install this project into your own, use `git clone` in your command-line.
+
+## Usage 
+To use this project, go to the cloned files, then type `node index` in your command-line. Be sure you’re in the correct location in your command-line/terminal first!
+
+## Contribution
+To contribute to this project, clone or fork the repo, make some edits in a new branch, then submit a PR. It’ll be merged to our main branch upon testing and approval.
+
+## Testing
+You can test this project with your own dummy data.
+
+## Questions
+Reach out to the repo owner, EmilyNecciai at enmayeski@gmail.com.
+
--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use(express.static('public'));
 //Generate Unique ID
 const generateUniqueId = require('generate-unique-id');
 
-
 //Create New Note
 function createNewNote(body, notesArray) {
     const note = body;
@@ -24,58 +23,36 @@ function createNewNote(body, notesArray) {
       JSON.stringify({ notes: notesArray }, null, 2)
     );
     return note;
-  }
-  
-//Validate note contents
-// function validateNote(note) {
-//     if (!note.title) {
-//       return false;
-//     }
-//     if (!note.text) {
-//       return false;
-//     }
-// };
-  
+  };  
 
 //Routes
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, './public/index.html'));
-//   });
-
 app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname, './public/index.html'));
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-  
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+  res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
-  // res.send("Test");
-    res.json(notes);
+  res.json(notes);
 });
 
-
 app.post('/api/notes', (req, res) => {
-      req.body.id = generateUniqueId();
-
-    // if (!validateNote(req.body)) {
-    //     res.status(400).send('You must enter both a title and a body.');
-    //   } else {
-        const note = createNewNote(req.body, notes);
-        res.json(note);
-      // }
-    });
+  req.body.id = generateUniqueId();
+  const note = createNewNote(req.body, notes);
+  res.json(note);
+});
     
+//Bonus - DELETE
+app.delete('/api/notes/:id', (req, res) => {
+  const { id } = req.params;
 
+  const delNote = notes.findIndex(note => note.id ==id);
 
-
-//Bonus WIP
-// app.delete('/api/notes/:id', (req, res) => {
-
-// })
-
+  notes.splice(delNote, 1);
+  return res.send();
+});
 
 //Listener
 app.listen(PORT, () => {
