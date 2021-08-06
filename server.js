1adbce14021b04baade77127e65303093f86adb0
Emily N Mayeski <enmayeski@gmail.com>
2021-08-05 23:24:20 -0400

    included route apis, potential for delete api, and potential for generating a unique id package. Not yet tested because of issues storing and pulling notes

--- server.js
+++ server.js
@@ -1,22 +1,79 @@
 // Dependencies & Express Data Parsing
+const fs = require('fs');
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
+const { notes } = require('./db/db');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+//Generate Unique ID
+const generateUniqueId = require('generate-unique-id');
+
+
+//Create New Note
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+      path.join(__dirname, './db/db.json'),
+      JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+  }
+  
+//Validate note contents
+// function validateNote(note) {
+//     if (!note.title) {
+//       return false;
+//     }
+//     if (!note.text) {
+//       return false;
+//     }
+// };
+  
 
 //Routes
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.html'));
+//   });
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
+res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
   
-  app.get('/notes', (req, res) => {
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
-  });
+});
+
+app.get('/api/notes', (req, res) => {
+    return res.json(notes);
+});
+
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = generateUniqueId();
+
+    // if (!validateNote(req.body)) {
+    //     res.status(400).send('You must enter both a title and a body.');
+    //   } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    //   }
+    });
+    
+
+
+
+//Bonus WIP
+// app.delete('/api/notes/:id', (req, res) => {
+
+// })
 
 
 //Listener
